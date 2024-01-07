019d13342443f16363e46684b65cd3502a7d800c
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2024-01-08 00:01:06 +0530

    Added resolvers

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ import { ApolloServer } from "@apollo/server";
 
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    game: (parent, args) => db.games.find((game) => game.id === args.id),
+    authors: () => db.authors,
+    author: (parent, args) =>
+      db.authors.find((author) => author.id === args.id),
+    reviews: () => db.reviews,
+    review: (parent, args) =>
+      db.reviews.find((review) => review.id === args.id),
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
