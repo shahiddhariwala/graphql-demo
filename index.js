930c8133b44b39b6708c35d26035cab295fe4a38
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2024-01-08 00:03:49 +0530

    Fixed: startStandaloneServer args

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ const server = new ApolloServer({
   },
 });
 
-const { url } = await startStandaloneServer({ server, listen: { port: 4000 } });
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
-console.log(`🚀 Server ready at ${url}:4000`);
+console.log(`🚀 Server ready at ${url}`);
